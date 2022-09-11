054c543493a173e3a717b81eab19e792a08f01d2
José Lucas Steinmetz da Silveira <65045856+joselucasSteinmetz@users.noreply.github.com>
2022-09-11 15:25:49 -0300

    update
    update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
-const express = require('express');
+ import express from "express";
 
 const app = express();
 
+app.use(express.json())
+
+const livros = [
+    {id: 1,"titulo": "Senhor dos Anéis"},
+    {id: 2, "titulo": "O hobbit"}
+]
+
 app.get('/', (req, res) => {
-    res.send('hello')
+    res.status(200).send('hello')
 })
+
+app.get('/livros', (req, res) => {
+    res.status(200).json(livros)
+})
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+    res.status(200).send('Livro cadastrado com sucesso')
+})
+
+export default app
